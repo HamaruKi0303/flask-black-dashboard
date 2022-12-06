56c4c5da1dc75b1e8a212b0e19ac5726e4a738bf
maki <haru.maki.ch.0303@gmail.com>
2022-12-06 23:34:18 +0900

    add sample app2

--- apps/home/app2.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Blueprint を作成
-bp = Blueprint('app2', __name__)
-
-# 先程 app.py にあった機能「@app.route('/hoge')」を app2.py に移動
-@bp.route('/hoge')
-def hoge():
-    return 'hoge5'
