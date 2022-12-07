9f08fd20d805b4f4cd4a3460f54830954c5ea086
maki <haru.maki.ch.0303@gmail.com>
2022-12-07 20:35:32 +0900

    add app5

---
+++ apps/home/sample/app5.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, url_for, request, redirect, Blueprint
+from datetime import datetime
+
+import pandas as pd
+import pprint
+from loguru import logger
+# Blueprint を作成
+bp = Blueprint('sample_app5', __name__)
+
+# /post にアクセスされ、GETもしくはPOSTメソッドでデータが送信された場合の処理
+@bp.route('/sample_app5', methods=['GET', 'POST'])
+def sample_app5():
+    
+    segment = "sample_app5"
+    # running_type = "develop"
+    running_type = "master"
+    
+    form_data_path = "apps/static/assets/data/form_data.csv"
+    df_form = pd.read_csv(form_data_path, index_col=0)
+    
+    # POSTメソッドの場合
+    if request.method == 'POST':
+
+        dict_form = request.form.to_dict()
+        df_form = df_form.append(dict_form, ignore_index=True)
+        
+        logger.info("df_form")
+        df_form.to_csv(form_data_path)
+        pprint.pprint(df_form)
+        
+    logger.info("dict_list_form")
+    dict_list_form = df_form.to_dict('records')
+    pprint.pprint(dict_list_form)
+    # name = request.form['name']
+    
+    return render_template('sample/app3.html', 
+                            dict_list_form=dict_list_form, 
+                            segment=segment, 
+                            running_type=running_type)
